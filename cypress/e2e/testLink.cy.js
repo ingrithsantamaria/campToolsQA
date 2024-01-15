5bea783d35c5b839421b4f6350325400edf349f1
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-01-15 15:53:06 -0500

    Link automation: Home Two, Created, No content, Moved

--- cypress/e2e/testLink.cy.js
+++ cypress/e2e/testLink.cy.js
@@ -1,14 +1,32 @@
 import { Link } from "../pages/link";
 const linkHome = new Link();
+function validationRedirect(object, something){
+    cy.window().then((winHome) => {
+        const numberWindowsBefore = winHome.window.length;
+        something.call(object);
+        cy.window().should("have.property", "length", numberWindowsBefore + 1);
+    })
+}
 describe("Should click on links", () => {
     beforeEach(() => {
         linkHome.navigateLink();
     })
     it("User clicks on the home link", () => {
-        cy.window().then((win) => {
-            const numberWindowsBefore = win.window.length;
-            linkHome.validateLinkHome();
-            cy.window().should("have.property", "length", numberWindowsBefore + 1);
-        })
+        validationRedirect(linkHome, linkHome.validateLinkHome)
+    })
+    it("User clicks on the home link two", () => {
+        validationRedirect(linkHome, linkHome.validateLinkHomeTwo)
+    })
+    it("User clicks on Created", () => {
+        linkHome.getLinkCreate()
+        linkHome.validateResponse()
+    })
+    it("User clicks on no Content", () => {
+        linkHome.getLinkNoContent()
+        linkHome.validateResponse()
+    })
+    it("User clicks on Moved", () => {
+        linkHome.getMoved()
+        linkHome.validateResponse()
     })
 })
--- cypress/pages/link.js
+++ cypress/pages/link.js
@@ -9,6 +9,27 @@ export class Link {
         return cy.get("#simpleLink")
     }
     validateLinkHome() {
-        this.getLinkHome().should("be.visible").click();
+        this.getLinkHome().should("exist").and("be.visible").click();
     }
+    getLinkHomeTwo(){
+        return cy.get("#dynamicLink")
+    }
+    validateLinkHomeTwo(){
+        this.getLinkHomeTwo().should("exist").and("be.visible").click();
+    }
+    getLinkCreate(){
+        cy.get("#created").should("exist").and("be.visible").click()
+    }
+    validateResponse(){
+        cy.get("#linkResponse").should("exist").and("be.visible")
+    }
+    getLinkNoContent(){
+        cy.get("#no-content").should("exist").and("be.visible").click();
+    }
+    getMoved(){
+        cy.get("#moved").should("exist").and("be.visible").click();
+    }
+    // getBadRequest(){
+
+    // }
 }
