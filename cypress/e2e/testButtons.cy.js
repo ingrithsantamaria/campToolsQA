bc3195924f971e9ea237abab3e78000f3da01786
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-01-15 08:56:02 -0500

    improvements given in feedback

--- cypress/e2e/testButtons.cy.js
+++ cypress/e2e/testButtons.cy.js
@@ -1,20 +1,16 @@
 import { Buttons } from "../pages/buttons";
 const buttons = new Buttons();
-describe("Double click on button", () => {
-    it ("Should doble click", () => {
+describe("User should click on the buttons", () => {
+    it ("Should doule click button", () => {
         buttons.navigateButtons();
         buttons.dobleClickOnButton();
         buttons.getMessageDblClickButton("You have done a double click");
-    })
-});
-describe("Click Right Button", () => {
-    it ("Should clcik to the right button", () => {
+    });
+    it ("Should click to the right button", () => {
         buttons.navigateButtons();
         buttons.clickRightOnButton();
         buttons.getMessageRightClick("You have done a right click");
-    })
-});
-describe("One click on button", () => {
+    });
     it("Should one click on button", () => {
         buttons.navigateButtons();
         buttons.getOneClickButton();
